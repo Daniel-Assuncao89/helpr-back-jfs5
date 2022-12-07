62b98fa73988cd498a282cfa3169c51db1790939
Beatriz Araújo <b.cordeiroaraujo@gmail.com>
2022-12-07 17:15:12 -0300

    Crianção do Usuário Controller e Usuário Service

--- src/main/java/org/soulcodeacademy/helpr/repositories/UsuarioRepository.java
+++ src/main/java/org/soulcodeacademy/helpr/repositories/UsuarioRepository.java
@@ -1,7 +1,9 @@
 package org.soulcodeacademy.helpr.repositories;
 
 import org.soulcodeacademy.helpr.domain.Usuario;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,6 +15,13 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
     Optional<Usuario> findByCpf(String cpf); // WHERE cpf = cpf
     List<Usuario> findByNome(String nome); // WHERE nome = nome
     List<Usuario> findByNomeContaining(String busca); // Filtrar pelo nome
+
+    @Query(value = "SELECT perfil FROM usuarios WHERE email = :email", nativeQuery = true)
+    Optional<Usuario> findPerfilByEmail(String email);
+
+    @Query( value = "SELECT perfil FROM usuarios WHERE id = :id", nativeQuery = true)
+    Optional<Usuario> findPerfilById(Integer id);
+
 }
 
 // Obs: Seguindo a convenção findBy o Spring implementa o método derivado/customizado
